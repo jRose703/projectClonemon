6906c4c814f1ccd9ab47a12a2705ae9155d2ebf6
Jessie <jessie.karabulut@gmail.com>
2023-05-06 20:35:58 +0200

    textBox is finished for now

--- src/Frames/battle/BattlePane.java
+++ src/Frames/battle/BattlePane.java
@@ -4,6 +4,7 @@ import Frames.textBox.*;
 
 import javax.swing.*;
 
+@SuppressWarnings({"FieldMayBeFinal", "FieldCanBeLocal"})
 public class BattlePane extends JLayeredPane{
 
 	private TestPanel testOne;
@@ -20,7 +21,7 @@ public class BattlePane extends JLayeredPane{
 		this.add(testTwo, Integer.valueOf(1));
 
 		this.textBox = new TextBox();
-		this.textBox.setMessage("Sein Panzer ist mit einer explosiven Schicht überzogen. Gegnerische Angriffe quittiert es gewaltig.");
+		this.textBox.setMessage("In computer programming, an anonymous function (function literal, lambda abstraction, lambda function, lambda expression or block) is a function definition that is not bound to an identifier. Anonymous functions are often arguments being passed to higher-order functions or used for constructing the result of a higher-order function that needs to return a function. If the function is only used once, or a limited number of times, an anonymous function may be syntactically lighter than using a named function. Anonymous functions are ubiquitous in functional programming languages and other languages with first-class functions, where they fulfil the same role for the function type as literals do for other data types.");
 		this.add(textBox, Integer.valueOf(2));
 
 		this.setVisible(false);
--- src/Frames/textBox/TextBox.java
+++ src/Frames/textBox/TextBox.java
@@ -1,28 +1,33 @@
 package Frames.textBox;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TextBox extends AbstractTextBox{
+@SuppressWarnings({"FieldMayBeFinal", "FieldCanBeLocal"})
+public class TextBox extends AbstractTextBox implements KeyListener{
 
     private final int FONT_SIZE = 30;  // this variable will change relative to frame size
-    private final int MAX_CHAR_SIZE = 39;  // this variable needs to stay a constant
-    private String message;
-    private List<String> lines;
+    private final int MAX_CHAR_SIZE = 36;  // this variable needs to stay a constant
+
+    private int row = 0;
+    private String message = "";
+    private List<String> lines = new ArrayList<>();
 
     public TextBox(){
         super();
-        message = "";
-        lines = new ArrayList<>();
+        this.setFocusable(true);
+        this.addKeyListener(this);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setFont(new Font("Arial", Font.BOLD, this.FONT_SIZE));
 
-        for(int i = 0; i < lines.size(); i++)
-            g.drawString(lines.get(i), 20, 20 + (20 * 2 * i) + this.FONT_SIZE);
+        for(int i = 0; i < 3 && i < lines.size(); i++)
+            g.drawString(lines.get(i + row), 20, 20 + (20 * 2 * i) + this.FONT_SIZE);
     }
 
     public void setMessage(String message) {
@@ -30,27 +35,56 @@ public class TextBox extends AbstractTextBox{
         createMessageList();
     }
 
-    public void createMessageList(){
+    private void createMessageList(){
         String[] words = this.message.split(" ");
-        String line = "";
+        StringBuilder line = new StringBuilder();
         for(int i = 0; i < words.length; i++){
             if(line.length() + words[i].length() < this.MAX_CHAR_SIZE)
-                line += words[i] + " ";
+                line.append(words[i]).append(" ");
             else{
-                lines.add(line);
-                line = words[i] + " ";
+                lines.add(line.toString());
+                line = new StringBuilder(words[i] + " ");
             }
             if(i == words.length - 1){
-                lines.add(line);
-                line = "";
+                lines.add(line.toString());
+                line = new StringBuilder();
             }
         }
-
-        // solely for testing
-        for(String text : lines){
-            System.out.println(text);
-        }
-
     }
+
+    private void resetTextBox(){
+        this.row = 0;
+        this.message = "";
+        this.lines.clear();
+        repaint();
+    }
+
+    // if the enter button is pressed
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == 10){
+
+            if(row < lines.size() - 3){
+                int difference = lines.size()-3 - row;
+                switch (difference) {
+                    case 1 -> row++;
+                    case 2 -> row = row + 2;
+                    default -> row = row + 3;
+                }
+                repaint();
+            }
+            else{
+                this.resetTextBox();
+                this.setVisible(false);
+            }
+        }
+    }
+
+    // These two methods are not needed in this case
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {}
 
 }
