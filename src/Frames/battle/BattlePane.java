e03b68b57ac78b81ac4afd8e306404f0deed2d15
Jessie <jessie.karabulut@gmail.com>
2023-05-06 22:37:50 +0200

    BattleMenuBox first prototype

--- src/Frames/battle/BattlePane.java
+++ src/Frames/battle/BattlePane.java
@@ -9,7 +9,7 @@ public class BattlePane extends JLayeredPane{
 
 	private TestPanel testOne;
 	private TestPanelTwo testTwo;
-	private TextBox textBox;
+	private BattleMenuBox battleBox;
 
 	public BattlePane() {
 		this.testOne = new TestPanel();
@@ -20,9 +20,8 @@ public class BattlePane extends JLayeredPane{
 		this.testTwo.setBounds(100, 100, 400, 400);
 		this.add(testTwo, Integer.valueOf(1));
 
-		this.textBox = new TextBox();
-		this.textBox.setMessage("In computer programming, an anonymous function (function literal, lambda abstraction, lambda function, lambda expression or block) is a function definition that is not bound to an identifier. Anonymous functions are often arguments being passed to higher-order functions or used for constructing the result of a higher-order function that needs to return a function. If the function is only used once, or a limited number of times, an anonymous function may be syntactically lighter than using a named function. Anonymous functions are ubiquitous in functional programming languages and other languages with first-class functions, where they fulfil the same role for the function type as literals do for other data types.");
-		this.add(textBox, Integer.valueOf(2));
+		this.battleBox = new BattleMenuBox();
+		this.add(battleBox, Integer.valueOf(2));
 
 		this.setVisible(false);
 		this.setLayout(null);
--- src/Frames/textBox/AbstractTextBox.java
+++ src/Frames/textBox/AbstractTextBox.java
@@ -8,9 +8,12 @@ import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
+@SuppressWarnings({"FieldMayBeFinal", "FieldCanBeLocal"})
 public abstract class AbstractTextBox extends JPanel{
 
-	public static final int TEXT_BOX_HEIGHT = 150;
+	public static final int TEXT_BOX_HEIGHT = BasicPanel.SCREENHEIGHT / 4;
+	public static final int TEXT_BOX_CENTER_X = BasicPanel.SCREENWIDTH / 2;
+	public static final int TEXT_BOX_CENTER_Y = TEXT_BOX_HEIGHT / 2;
 	
 	private Border blackline = BorderFactory.createLineBorder(Color.black);
 	public static Border compound = BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createLoweredBevelBorder());
@@ -20,7 +23,7 @@ public abstract class AbstractTextBox extends JPanel{
 		compound = BorderFactory.createCompoundBorder(blackline, compound);
 
 		this.setBorder(compound);
-		this.setBounds(0, 450, BasicPanel.SCREENWIDTH, TEXT_BOX_HEIGHT);
+		this.setBounds(0, BasicPanel.SCREENHEIGHT * 3 / 4, BasicPanel.SCREENWIDTH, TEXT_BOX_HEIGHT);
 		this.setBackground(Color.WHITE);
 		this.setVisible(true);
 		this.setLayout(null);
