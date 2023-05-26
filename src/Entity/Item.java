39026744bd8fca785486295f34eab13b3ca2655c
Dominik Fritsche <dominik.fritsche@mailbox.tu-dresden.de>
2023-05-26 11:10:47 +0200

    inventory mvp

--- src/Entity/Item.java
+++ src/Entity/Item.java
@@ -1,5 +1,34 @@
 package Entity;
 
 public class Item {
-    public Item(){}
+    public String Type = "";
+    public int amount = 0;
+
+    public String getType() {
+        return Type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Item(String Type){
+        this.Type = Type;
+        this.amount=1;
+    }
+    public void add_Item(){
+        this.amount ++;
+    }
+    public void add_Item(int amount){
+        this.amount += amount;
+    }
+    public void remove_Item(){
+        if (this.amount>1){
+            amount--;
+        }
+        else{
+            //TODO: delete item from inventory
+        }
+    }
+
 }
--- src/Entity/PlayerEntity.java
+++ src/Entity/PlayerEntity.java
@@ -1,12 +1,25 @@
 package Entity;
 
+import java.util.Objects;
+import java.util.Vector;
+
 public class PlayerEntity extends Entity {
-    public Item[] Inventory = new Item[50];
-    public Item[] getInventory(){
+    public Vector<Item> Inventory = new Vector<>();
+    public Vector<Item> getInventory(){
         return this.Inventory;
     }
     public void addToInventory(Item item_given){
-        //TODO
+        boolean done = false;
+        for (Item item : this.Inventory) {
+            if (Objects.equals(item.getType(), item_given.getType())) {
+                item.add_Item();
+                done = true;
+                break;
+            }
+        }
+        if (!done){
+            this.Inventory.add(item_given);
+        }
     }
 
     @Override
