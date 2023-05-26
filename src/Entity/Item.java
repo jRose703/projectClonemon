83b41d0ede47ef8956e167956424076c954e571a
Dominik Fritsche <dominik.fritsche@mailbox.tu-dresden.de>
2023-05-26 14:23:17 +0200

    mark battle-items

--- src/Entity/Item.java
+++ src/Entity/Item.java
@@ -3,6 +3,7 @@ package Entity;
 public class Item {
     public String Type = "";
     public int amount = 0;
+    public boolean battle = false;
 
     public String getType() {
         return Type;
@@ -12,9 +13,10 @@ public class Item {
         return amount;
     }
 
-    public Item(String Type){
+    public Item(String Type, boolean offensive){
         this.Type = Type;
         this.amount=1;
+        this.battle = offensive;
     }
     public void add_Item(){
         this.amount ++;
@@ -26,6 +28,10 @@ public class Item {
         if (this.amount>1){
             amount--;
         }
+    }
+
+    public boolean getBattle(){
+        return this.battle;
     }
 
 }
