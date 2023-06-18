d5aad5a916d5a9bfe88e09a51be4d46a871c2bc1
Dominik Fritsche <dominik.fritsche@mailbox.tu-dresden.de>
2023-06-18 17:20:37 +0200

    add Fightersimages

--- src/BattleSystem/Fighters/Grass.java
+++ src/BattleSystem/Fighters/Grass.java
@@ -3,12 +3,14 @@ package BattleSystem.Fighters;
 import BattleSystem.Fighter;
 import Frames.BattleUI.BattleParticipant;
 
+import javax.swing.*;
+
 public class Grass extends Fighter {
-    private imageIcon BackSprite;
-    private imageIcon FrontSprite;
-    public Grass(String name, BattleParticipant battleParty, int ID, int maxHP, int attackStat, int defenseStat, int initStat, imageIcon backSprite, imageIcon fronSprite){
+    private ImageIcon BackSprite;
+    private ImageIcon FrontSprite;
+    public Grass(String name, BattleParticipant battleParty, int ID, int maxHP, int attackStat, int defenseStat, int initStat){
         super(name, battleParty, ID, maxHP, attackStat, defenseStat,initStat);
-        this.BackSprite = backSprite;
-        this.FrontSprite = fronSprite;
+        this.BackSprite = new ImageIcon("assets/Fighter/grass_back.png");
+        this.FrontSprite = new ImageIcon("assets/Fighter/grass_front.png");
     }
 }
