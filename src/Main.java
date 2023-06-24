bf64936f714abfeb46c21c7cc1a8090d3bbea176
Jessie <jessie.karabulut@gmail.com>
2023-06-24 12:19:40 +0200

    cleaned the main file

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
-import ReadAndWrite.WorldOperations.ReadWorldFromJson;
-import ReadAndWrite.WorldOperations.WriteWorldToJson;
-import Worlds.World;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        //World world = ReadWorldFromJson.readWorldFromFile("world");
-        //WriteWorldToJson.saveWorld(world,"world");
-
-
-
         StateMachine stateMachine = new StateMachine();
+
     }
 }
